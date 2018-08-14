516d817cce03abe6149fb2a8d21cb70fb70e0de8
udayaditya singh <udayaditya.singh@gmail.com>
2018-08-14 15:53:24 +0530

    first

---
+++ server.js
@@ -0,0 +1,5 @@
+const base64url = require('base64url');
+
+base64url.fromBase64("DFBLC8TH2a4nmDHaXUTBpZQzo872gwxjdAkGmc6rR8g=");
+
+console.log("output : ",base64url.fromBase64("DFBLC8TH2a4nmDHaXUTBpZQzo872gwxjdAkGmc6rR8g="));
